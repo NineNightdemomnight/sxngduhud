9b137ef860f89bc70272e281994646d092adc731
NineNightdemomnight <42994065+NineNightdemomnight@users.noreply.github.com>
2024-07-01 20:23:59 +0700

    Update mainui.lua

--- mainui.lua
+++ mainui.lua
@@ -1,7 +1,7 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Consistt/Ui/main/UnLeaked"))()
 --local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/NineNightdemomnight/cloneui/main/cloneui"))()
 
-library.rank = "developer"
+library.rank = "FREE VIP  🟢 "
 local Wm = library:Watermark("BLUENIGHT example | v" .. library.version ..  " | " .. library:GetUsername() .. " | rank: " .. library.rank)
 local FpsWm = Wm:AddWatermark("fps: " .. library.fps)
 coroutine.wrap(function()
@@ -17,21 +17,33 @@ for i = 20,0,-1 do
     local LoadingXSX = Notif:Notify("Loading BLUENIGHT lib v2, please be patient.", 3, "information") -- notification, alert, error, success, information
 end 
 
-library.title = "BLUENIGHT"
+library.title = "BLUENIGHT HUB"
 
 library:Introduction()
 wait(1)
 local Init = library:Init()
 
-local Tab1 = Init:NewTab("Example tab")
+local Tab1 = Init:NewTab("BLUENIGHT tab")
 
-local Section1 = Tab1:NewSection("TSET Components")
+local Section1 = Tab1:NewSection("BLUENIGHT Components")
 
 -- Adding a toggle to enable/disable the ESP script
-local ESP_Toggle = Tab1:NewToggle("Enable ESP Script", false, function(value)
+local ESP_Toggle = Tab1:NewToggle("ตัวเรื่องแสง / ESP light 🔆 ctrl+h ", false, function(value)
     if value then
         -- Run the ESP script if the toggle is enabled
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/NineNightdemomnight/esp/main/esp.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/NineNightdemomnight/sxngduhud/main/esp.lua"))()
+    else
+        -- Disable ESP script (You might need to implement a way to stop the ESP script if it's running)
+        -- Example: Clear ESP objects or deactivate the script
+        print("ตัวเรื่องแสง / ESP light 🔆 ctrl+h ")
+    end
+end)
+
+
+local ESPBAR_Toggle = Tab1:NewToggle(" ชื่อพร้อมเลือดระยะ / ESP BAR 🧊 ctrl+b ", false, function(value)
+    if value then
+        -- Run the ESP script if the toggle is enabled
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/NineNightdemomnight/sxngduhud/main/espbar.lua"))()
     else
         -- Disable ESP script (You might need to implement a way to stop the ESP script if it's running)
         -- Example: Clear ESP objects or deactivate the script
@@ -40,10 +52,10 @@ local ESP_Toggle = Tab1:NewToggle("Enable ESP Script", false, function(value)
 end)
 
 
-local ESPBAR_Toggle = Tab1:NewToggle("Enable ESP BAR Script", false, function(value)
+local ESPFRAME_Toggle = Tab1:NewToggle(" กรอบ / ESP FRAME 🧊 ctrl+f  ", false, function(value)
     if value then
         -- Run the ESP script if the toggle is enabled
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/NineNightdemomnight/esp/main/hpbar.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/NineNightdemomnight/sxngduhud/main/espframe.lua"))()
     else
         -- Disable ESP script (You might need to implement a way to stop the ESP script if it's running)
         -- Example: Clear ESP objects or deactivate the script
@@ -52,18 +64,36 @@ local ESPBAR_Toggle = Tab1:NewToggle("Enable ESP BAR Script", false, function(va
 end)
 
 
+local ESPLINE_Toggle = Tab1:NewToggle(" เส้นบะหมี้ / ESP LINE 🧊 ctrl+L  ", false, function(value)
+    if value then
+        -- Run the ESP script if the toggle is enabled
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/NineNightdemomnight/sxngduhud/main/espline.lua"))()
+    else
+        -- Disable ESP script (You might need to implement a way to stop the ESP script if it's running)
+        -- Example: Clear ESP objects or deactivate the script
+        print("ESP Script disabled")
+    end
+end)
 
-
-
+local ESPLINE_Toggle = Tab1:NewToggle(" วาป / TP 🧊   ", false, function(value)
+    if value then
+        -- Run the ESP script if the toggle is enabled
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/NineNightdemomnight/sxngduhud/main/tp.lua"))()
+    else
+        -- Disable ESP script (You might need to implement a way to stop the ESP script if it's running)
+        -- Example: Clear ESP objects or deactivate the script
+        print("ESP Script disabled")
+    end
+end)
 
 
 
 
 -- Adding a toggle to enable/disable the Fly script
-local Fly_Toggle = Tab1:NewToggle("Fly", false, function(value)
+local Fly_Toggle = Tab1:NewToggle("บิน / Fly  🦋", false, function(value)
     if value then
         -- Run the Fly script if the toggle is enabled
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/NineNightdemomnight/esp/main/fly.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/NineNightdemomnight/sxngduhud/main/fly.lua"))()
     else
         -- Disable Fly script (You might need to implement a way to stop the Fly script if it's running)
         -- Example: Clear Fly related changes or deactivate the script
@@ -72,26 +102,17 @@ local Fly_Toggle = Tab1:NewToggle("Fly", false, function(value)
 end)
 
 -- Adding a toggle to enable/disable the Aimbot script
-local Aimbot_Toggle = Tab1:NewToggle("Aimbot", false, function(value)
+local Aimbot_Toggle = Tab1:NewToggle("ล็อกหัว เบต้า/Aimbot beta 👤 ctrl+Q", false, function(value)
     if value then
         -- Run the Aimbot script if the toggle is enabled
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/NineNightdemomnight/esp/main/aimbottest"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/NineNightdemomnight/sxngduhud/main/aimbot"))()
     else
         -- Disable Aimbot script (You might need to implement a way to stop the Aimbot script if it's running)
         print("Aimbot Script disabled")
     end
 end)
 
--- Adding a toggle to enable/disable the TP script
-local TP_Toggle = Tab1:NewToggle("TP Script", false, function(value)
-    if value then
-        -- Run the TP script if the toggle is enabled
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/NineNightdemomnight/esp/main/tp.lua"))()
-    else
-        -- Disable TP script (You might need to implement a way to stop the TP script if it's running)
-        print("TP Script disabled")
-    end
-end)
+
 
 local Label1 = Tab1:NewLabel("Example label", "left")--"left", "center", "right"
 
@@ -133,4 +154,4 @@ local Slider1 = Tab1:NewSlider("Slider 1", "", true, "/", {min = 1, max = 100, d
     print(value)
 end)
 
-local FinishedLoading = Notif:Notify("Loaded xsx example", 4, "success")
+local FinishedLoading = Notif:Notify("Loaded BLUENIGHT example", 4, "success")
